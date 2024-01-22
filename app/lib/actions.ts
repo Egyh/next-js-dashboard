ce90aedf24a6905945022a0bd881fcab13c04c66
Hamaguchi <hamaguchi.fundely@gmail.com>
2024-01-22 10:44:30 +0900

    feat: chap13 Handling Errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -26,10 +26,17 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
+
+try {
   await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+  `; 
+} catch (error) {
+  return {
+    message: 'Database Error: Failed to Create Invoice.',
+  };
+}
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
@@ -56,7 +63,15 @@ export async function updateInvoice(id: string, formData: FormData) {
     redirect('/dashboard/invoices');
   }
 
-export async function deleteInvoice(id: string) {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath('/dashboard/invoices');
-  }  
+  export async function deleteInvoice(id: string) {
+    throw new Error('Failed to Delete Invoice');
+   
+    // Unreachable code block
+    try {
+      await sql`DELETE FROM invoices WHERE id = ${id}`;
+      revalidatePath('/dashboard/invoices');
+      return { message: 'Deleted Invoice' };
+    } catch (error) {
+      return { message: 'Database Error: Failed to Delete Invoice' };
+    }
+  }
